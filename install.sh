00162e62b4cd11e7c2d2c8b2f38a51e35a5e5759
shashi29 <shashi.raj30@yahoo.in>
2025-01-03 14:50:23 +0000

    Add requirements.txt

---
+++ install.sh
@@ -0,0 +1,5 @@
+wget https://repo.anaconda.com/archive/Anaconda3-2024.10-1-Linux-x86_64.sh
+bash Anaconda3-2024.10-1-Linux-x86_64.sh -b -p $HOME/anaconda3
+
+#Add this path in bashrc
+export PATH="$HOME/anaconda3/bin:$PATH"
